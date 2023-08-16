0315623ebaefb18d9dcce8f2b2f52f2247486d71
Rashmi Gunawardana <90468021+RashmiNirasha@users.noreply.github.com>
2023-08-16 23:23:14 +0530

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,28 +1,23 @@
 # Import libraries
-
 import argparse
 import glob
 import os
-
 import pandas as pd
-
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-
-# define functions
+# Define functions
 def main(args):
     # TO DO: enable autologging
 
-
-    # read data
+    # Read data
     df = get_csvs_df(args.training_data)
 
-    # split data
+    # Split data
     X_train, X_test, y_train, y_test = split_data(df)
 
-    # train model
+    # Train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
-
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -32,43 +27,46 @@ def get_csvs_df(path):
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
+def split_data(df):
+    # Separate features (X) and target (y)
+    X = df.drop(columns=["target"])
+    y = df["target"]
 
-# TO DO: add function to split data
+    # Split data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # train model
+    # Train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
-
 def parse_args():
-    # setup arg parser
+    # Setup arg parser
     parser = argparse.ArgumentParser()
 
-    # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+    # Add arguments
+    parser.add_argument("--training_data", dest='training_data', type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
 
-    # parse args
+    # Parse args
     args = parser.parse_args()
 
-    # return args
+    # Return args
     return args
 
-# run script
+# Run script
 if __name__ == "__main__":
-    # add space in logs
+    # Add space in logs
     print("\n\n")
     print("*" * 60)
 
-    # parse args
+    # Parse args
     args = parse_args()
 
-    # run main function
+    # Run main function
     main(args)
 
-    # add space in logs
+    # Add space in logs
     print("*" * 60)
     print("\n\n")
