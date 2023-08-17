94500bb57235c032882e03e12bd43c4df0a5643e
Rashmi Gunawardana <90468021+RashmiNirasha@users.noreply.github.com>
2023-08-17 16:27:26 +0530

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,92 +1,107 @@
-# Import libraries
-import argparse
-import glob
-import os
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+# import libraries
 import mlflow
+import argparse
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt
 
-# Define functions
 def main(args):
-    # Enable autologging
+	# TO DO: enable autologging
     mlflow.autolog()
 
-    # Read data
-    df = get_csvs_df(args.training_data)
 
-    # Split data
+    # read data
+    df = get_data(args.training_data)
+
+    # split data
     X_train, X_test, y_train, y_test = split_data(df)
 
-    # Train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    # train model
+    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
-def get_csvs_df(path):
-    if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = glob.glob(f"{path}/*.csv")
-    if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+    # evaluate model
+    eval_model(model, X_test, y_test)
 
+# function that reads the data
+def get_data(path):
+    print("Reading data...")
+    df = pd.read_csv(path)
+
+    
+    return df
+
+# function that splits the data
 def split_data(df):
-    # Separate features (X) and target (y)
-    X = df.drop(columns=["target"])
-    y = df["target"]
-
-    # Split data into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+    print("Splitting data...")
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
+    
 
+    
+
+# function that trains the model
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # Start an MLflow run
-    with mlflow.start_run() as run:
-        # Log parameters
-        mlflow.log_param("reg_rate", reg_rate)
+    print("Training model...")
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
-        # Train model
-        model = LogisticRegression(C=1/reg_rate, solver="liblinear")
-        model.fit(X_train, y_train)
+    return model
 
-        # Log metrics
-        train_score = model.score(X_train, y_train)
-        test_score = model.score(X_test, y_test)
-        mlflow.log_metric("train_score", train_score)
-        mlflow.log_metric("test_score", test_score)
+# function that evaluates the model
+def eval_model(model, X_test, y_test):
+    # calculate accuracy
+    y_hat = model.predict(X_test)
+    acc = np.average(y_hat == y_test)
+    print('Accuracy:', acc)
 
-        # Save the model
-        mlflow.sklearn.save_model(model, "model")
+    # calculate AUC
+    y_scores = model.predict_proba(X_test)
+    auc = roc_auc_score(y_test,y_scores[:,1])
+    print('AUC: ' + str(auc))
+
+    # plot ROC curve
+    fpr, tpr, thresholds = roc_curve(y_test, y_scores[:,1])
+    fig = plt.figure(figsize=(6, 4))
+    # Plot the diagonal 50% line
+    plt.plot([0, 1], [0, 1], 'k--')
+    # Plot the FPR and TPR achieved by our model
+    plt.plot(fpr, tpr)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
 
 def parse_args():
-    # Setup arg parser
+    # setup arg parser
     parser = argparse.ArgumentParser()
 
-    # Add arguments
-    parser.add_argument("--training_data", dest='training_data', type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
+    # add arguments
+    parser.add_argument("--training_data", dest='training_data',
+                        type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate',
+                        type=float, default=0.01)
 
-    # Parse args
+    # parse args
     args = parser.parse_args()
 
-    # Return args
+    # return args
     return args
 
-# Run script
+# run script
 if __name__ == "__main__":
-    # Add space in logs
+    # add space in logs
     print("\n\n")
     print("*" * 60)
 
-    # Parse args
+    # parse args
     args = parse_args()
 
-    # Initialize MLflow
-    mlflow.set_tracking_uri("your_mlflow_tracking_uri")
-
-    # Run main function
+    # run main function
     main(args)
 
-    # Add space in logs
+    # add space in logs
     print("*" * 60)
     print("\n\n")
